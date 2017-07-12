20bd572e08ec956cbdb3dd2f60b0559b8e175f12
thyagorafaell <thyagorafael012@gmail.com>
2017-07-12 20:57:16 -0300

    Cria camada de persistência

---
+++ src/js/lib/storage.js
@@ -0,0 +1,21 @@
+const appPrefix = '__se__todo__app__';
+
+export function save(entity, data) {
+    
+};
+
+export function get(entity, id) {
+    
+};
+
+export function update(entity, id) {
+    
+};
+
+export function delete(entity, id) {
+  
+};
+
+function getAll(entity) {
+    
+};
