bf136d6806eb9d9c908f2e15f37e680febc59b56
thyagorafaell <thyagorafael012@gmail.com>
2017-07-12 23:01:25 -0300

    Remove migração do main

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,10 @@ import '../css/main.css';
 import {todos, filters} from './state';
 import {render} from './view';
 import {registerEventHandlers} from './events';
-import {run} from './migrations/V1';
 
 todos.subscribe(newState => render(document.body, newState));
 filters.subscribe(newState => render(document.body, newState));
 
-run();
 render(document.body, todos.getState(), filters.getState());
 
 registerEventHandlers();
