b6b75b0414053c7c41f3a02d65b4da1039587d4e
thyagorafaell <thyagorafael012@gmail.com>
2017-07-12 22:04:28 -0300

    Métodos CRUD da camada de persistência

--- src/js/lib/storage.js
+++ src/js/lib/storage.js
@@ -1,21 +1,58 @@
+import * as uuid from 'uuid-v4';
+
 const appPrefix = '__se__todo__app__';
 
-export function save(entity, data) {
-    
+function getEntityName(entity) {
+    return appPrefix + entity;
 };
 
 export function get(entity, id) {
+    entity = getEntityName(entity);
+    let data = getAll(entity);
     
+    if (!id) {
+        return data;
+    }
+    
+    return data.find(item => {
+        return item.id === id;
+    });
 };
 
-export function update(entity, id) {
+export function save(entity, item) {
+    entity = getEntityName(entity);
+    item.id = uuid.default();
     
+    let data = getAll(entity);
+    data.push(item);
+    
+    localStorage.setItem(entity, data);
+    return item;
+};
+
+export function update(entity, item) {
+    entity = getEntityName(entity);
+    item.id = uuid.default();
+
+    let data = getAll(entity).map(data => {
+        if (data.id === item.id) {
+            return item;
+        }
+
+        return data;
+    });
+
+    localStorage.setItem(entity, data);
+    return item;
 };
 
 export function delete(entity, id) {
-  
-};
+    entity = getEntityName(entity);
 
-function getAll(entity) {
-    
+    let data = getAll(entity).filter(item => {
+        return item.id !== id;
+    });
+
+    localStorage.setItem(entity, data);
+    return data;
 };
